205efd79ede80762c91369bdd1f300f6050664cb
spobb <gverlaeken@gmail.com>
2024-12-16 00:29:01 +0100

    added wall collisions

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext('2d');
 const score = document.getElementById('score');
 
 // pixels per cell
-let cellSize = 32;
+let cellSize = 48;
 let width = canvas.width / cellSize;
 let height = canvas.height / cellSize;
 let defeated = false;
@@ -29,12 +29,13 @@ class Snake {
 
         this.x += this.direction.x;
         this.y += this.direction.y;
+        this.CheckCollision();
     }
     Draw() {
         ctx.fillStyle = '#9f2';
         ctx.fillRect((snake.x * cellSize) + 2, (snake.y * cellSize) + 2, cellSize - 4, cellSize - 4);
         for (let i = 0; i < this.segments.length; i++) {
-            ctx.fillStyle = i == 0 ? '#c92' : '#9f2'
+            ctx.fillStyle = i == 0 ? '#9a7352' : '#9f2'
             ctx.fillRect((this.segments[i][0] * cellSize) + 4, (this.segments[i][1] * cellSize) + 4, cellSize - 8, cellSize - 8);
         }
     }
@@ -43,6 +44,13 @@ class Snake {
         if (this.segments.includes(nextPos)) {
             defeated = true;
         }
+    }
+
+    CheckCollision() {
+        if (this.x >= width) this.x = 0;
+        if (this.y >= height) this.y = 0;
+        if (this.x < 0) this.x = width - 1;
+        if (this.y < 0) this.y = height - 1;
     }
 }
 class Apple {
@@ -60,7 +68,7 @@ const spawnApple = () => {
 }
 
 const snake = new Snake(this.x = width / 2, this.y = height / 2)
-let apple = new Apple(Math.round(Math.random() * (width)), Math.round(Math.random() * height))
+let apple = new Apple(Math.round(Math.random() * (width)), Math.round(Math.random() * (height)))
 
 // listen to movement keys
 document.addEventListener('keydown', k => {
@@ -84,7 +92,8 @@ setInterval(() => {
     snake.Move();
     snake.CheckDefeat();
     ctx.fillStyle = '#f33';
-    ctx.fillRect(apple.x * cellSize, apple.y * cellSize, cellSize, cellSize);
+    ctx.fillRect((apple.x * cellSize) + 4, (apple.y * cellSize) + 4, cellSize - 8, cellSize - 8);
     score.innerText = ('000' + snake.segments.length).slice(-3);
 
-}, 1000 / 8);
+}, 1000 / 8
+);
